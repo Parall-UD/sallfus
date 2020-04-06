61e99b76a4ab0e93a1d08121fc4e5ce01e4f4539
Parall-UD <49329081+Parall-UD@users.noreply.github.com>
2020-04-06 17:53:54 -0500

    Delete prueba.py

--- sallfus/fusion/prueba.py
+++
@@ -1,7 +0,0 @@
-from broveyCPU import fusion_images
-import skimage.io
-
-m = skimage.io.imread('D:/Users/Andres/Downloads/1024rgb.tif', plugin='tifffile')
-p = skimage.io.imread('D:/Users/Andres/Downloads/1024pan.tif', plugin='tifffile')
-
-res = fusion_images(m,p,'D:/Users/Andres/Documents/figLib',True)
